62b47f5477920007f0d4a74ee635bf6d68b82781
System Administrator <45667758+michaelhofrichter@users.noreply.github.com>
2020-07-31 09:01:17 -0500

    update

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.TXRequestForSubpeona',
-      version='0.0.1',
+      version='0.0.2',
       description=('A docassemble extension.'),
       long_description='Originally designed for the 2020 Texas Poverty Law Conference.',
       long_description_content_type='text/markdown',
